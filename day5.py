5b70758c268b07d2a5a46c020896c9ee653fbe99
Connor Lane Smith <cls@lubutu.com>
2020-12-05 14:08:29 +0000

    Day 5 simplifications

--- day5.py
+++ day5.py
@@ -1,37 +1,21 @@
 import sys
 
-num_seats = 128 * 8
-
 def boarding_pass_seat(boarding_pass):
-    lower = 0
-    upper = num_seats
-    delta = num_seats // 2
+    seat = 0
 
     for letter in boarding_pass:
-        if letter in ('B', 'R'):
-            lower += delta
-        else:
-            upper -= delta
-        delta //= 2
+        seat <<= 1
+        seat |= letter in ('B', 'R')
 
-    assert lower == upper - 1
+    return seat
 
-    return lower
-
-seats = set()
-
-for line in sys.stdin:
-    seat = boarding_pass_seat(line.strip())
-    seats.add(seat)
+seats = set(boarding_pass_seat(line.strip()) for line in sys.stdin)
 
 print(max(seats))
 
-middle = False
+seat = min(seats)
 
-for seat in range(num_seats):
-    if seat in seats:
-        middle = True
-    elif middle:
-        break
+while seat in seats:
+    seat += 1
 
 print(seat)
